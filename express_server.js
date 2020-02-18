6f6e3b4625e97b268c2eb7fcaa8961bb932cb8b5
KateRaymundi <kateacristina@outlook.com>
2020-02-18 23:18:32 +0000

    added deleting and edit to tintapp

--- express_server.js
+++ express_server.js
@@ -13,7 +13,6 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-
 app.get("/urls.json", (req, res) => { 
   res.json(urlDatabase);
 });
@@ -49,19 +48,6 @@ app.post("/urls", (req, res) => { // long URL referes do the body to the request
   res.redirect(`/urls/${newKey}`)
 });
 
-app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL]
-  res.redirect(longURL);
-});
-
-app.post("/urls/:shortURL/delete", (req, res) => { // added
-  console.log(urlDatabase[req.params.shortURL]) // added
-
-  delete urlDatabase[req.params.shortURL]
-  res.redirect('/urls')
-
-})
-
 function generateRandomString() {
   var result           = '';
   var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -70,6 +56,27 @@ function generateRandomString() {
   }
   return result;
 }
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
+});
+
+app.post("/urls/:shortURL/delete", (req, res) => { // Essa parte aqui foi criada para poder deletar as shorts URL
+  console.log(urlDatabase[req.params.shortURL]) // que ficam no site. Junto com isso eu tambem preciso 
+                                                // acrescentar o form no urls_index 
+  delete urlDatabase[req.params.shortURL]
+  res.redirect('/urls')
+
+})
+
+app.post("/urls/:shortURL", (req, res) => { // cria um novo field para o usuario poder editar um novo 
+  console.log(urlDatabase[req.params.shortURL]) // endereco de web. 
+
+  urlDatabase[req.params.shortURL] = req.body.longURL // a variable usada aqui eh o object shortURL que passa como variavel a longo URL
+  res.redirect('/urls')
+
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
