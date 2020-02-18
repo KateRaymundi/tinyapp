4e988e239efb1e0adb335ded784909bd35a4e967
KateRaymundi <kateacristina@outlook.com>
2020-02-18 01:05:40 +0000

    ramdom function added

--- express_server.js
+++ express_server.js
@@ -2,13 +2,17 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-app.set('views', './view'); // esse codigo pode me dar problema no futuro
+app.set('views', './view'); // esse codigo pode me dar problema no futuroviews
 app.set("view engine", "ejs");
+
+const bodyParser = require("body-parser");       //??????
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
 
 app.get("/urls.json", (req, res) => { 
   res.json(urlDatabase);
@@ -27,10 +31,21 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
