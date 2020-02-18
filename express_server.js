f8cb8abf7896bcceb4808ce161244b6cc08a23a9
KateRaymundi <kateacristina@outlook.com>
2020-02-18 19:43:09 +0000

    added delete bottom

--- express_server.js
+++ express_server.js
@@ -35,7 +35,7 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-app.get("/urls/:shortURL", (req, res) => {
+app.get("/urls/:shortURL", (req, res) => { //shortURL is id in this case
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
@@ -53,6 +53,14 @@ app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL]
   res.redirect(longURL);
 });
+
+app.post("/urls/:shortURL/delete", (req, res) => { // added
+  console.log(urlDatabase[req.params.shortURL]) // added
+
+  delete urlDatabase[req.params.shortURL]
+  res.redirect('/urls')
+
+})
 
 function generateRandomString() {
   var result           = '';
